36502e66db0845f165f2f80808f7dc14b5025738
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-15 13:23:43 +0545

    -added intital files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+<h2> discord clone</h2>
+    </div>
+  );
+}
+
+export default App;
