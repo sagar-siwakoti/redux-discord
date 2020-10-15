2124f7ce8e8dd95699ffb3d4d557ae6629489f64
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-15 20:18:44 +0545

    -chat message box fix

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./Chat1.css";
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import ChatHeader from "./ChatHeader";
 function Chat(props) {
     return (
         <div className="chat">
             <ChatHeader/>
+            <div className="chat__messages"></div>
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large"/>
+                <form>
+                    <input placeholder={`Message #TESTCHANNEL`}/>
+                    <button className="chat__inputButton" type="submit">Send Message</button>
+                </form>
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large"/>
+                    <GifIcon fontSize="large"/>
+                    <EmojiEmotionsIcon fontSize="large"/>
+                </div>
+            </div>
         </div>
     );
 }
