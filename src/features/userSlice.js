b147b6551a032df802faafee1df95c62d77aa523
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-16 12:54:57 +0545

    -redux store setup

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,37 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
-    value: 0,
+    user: null,
   },
   reducers: {
-    login: (state,action) => {
-
-      state.value += 1;
+    login: (state, action) => {
+      state.user += action.payload;
     },
-    logout:(state)=>{
-      state.user=null
-    }
-
+    logout: (state) => {
+      state.user = null;
+    },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const { login,logout } = userSlice.actions;
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-export const incrementAsync = amount => dispatch => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
-};
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.user.value)`
-export const selectCount = state => state.user.value;
+export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
