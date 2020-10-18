0451d33e3b1f4df7a1856446b0d52a70a88e281c
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-18 16:38:26 +0545

    -dispach bug fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, {useEffect} from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {login, logout, selectUser} from "./features/userSlice";
 import Login from "./components/Login/Login";
+import {auth} from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
-  useEffect()
+  const dispatch=useDispatch(selectUser);
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+      console.log("user is ", authUser)
+      if (authUser){
+        //user is logged in
+        dispatch(login({
+          uid:authUser.uid,
+          photo:authUser.photoURL,
+          email:authUser.email,
+          displayName:authUser.displayName
+        }))
+      }else {
+        //user is not logged in
+        dispatch(logout());
+      }
+    })
+  },[dispatch])
   return (
     <div className="app">
       {user ? (
