d727420fafc0da37c630bb7b3a1f7b8911652972
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-18 17:26:17 +0545

    -logout bug fix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
-import CallIcon from '@material-ui/icons/Call';
-import MicIcon from '@material-ui/icons/Mic';
-import HeadsetIcon from '@material-ui/icons/Headset';
-import SettingsIcon from '@material-ui/icons/Settings';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from "@material-ui/icons/Call";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
-import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
 import SidebarChannel from "./SidebarChannel";
 import Avatar from "@material-ui/core/Avatar";
-import {useSelector} from "react-redux";
-import {selectUser} from "../../features/userSlice";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 import Button from "@material-ui/core/Button";
+import { auth } from "../../firebase";
 
 function Sidebar(props) {
-    const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
 
-    return (
+  return (
     <div className="sidebar">
       <div className="sidebar__top">
         <h3>Epic Dudes</h3>
@@ -45,29 +46,32 @@ function Sidebar(props) {
           className="sidebar__voiceIcon"
         />
         <div className="sidebar__voiceInfo">
-            <h3>Voice Connected</h3>
-            <p>Stream</p>
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
         </div>
-          <div className="sidebar__voiceIcons">
-              <InfoOutlinedIcon/>
-              <CallIcon/>
-          </div>
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
       </div>
-        <div className="sidebar__profile">
-            <Avatar src={user.photo}/>
-            <div className="sidebar__profileInfo">
-                <h3>{user.displayName}</h3>
-                <p>#{user.uid.substr(0,5)}</p>
-            </div>
-            <div className="sidebar__profileIcons">
-                <MicIcon/>
-                <HeadsetIcon/>
-                <SettingsIcon/>
-            </div>
+      <div className="sidebar__profile">
+        <Avatar src={user.photo} />
+        <div className="sidebar__profileInfo">
+          <h3>{user.displayName}</h3>
+          <p>#{user.uid.substr(0, 5)}</p>
         </div>
-            <div className="sidebar__logout">
-                <Button>LOGOUT<PowerSettingsNewIcon/></Button>
-            </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
+        </div>
+      </div>
+      <div className="sidebar__logout">
+        <Button onClick={() => auth.signOut()}>
+          LOGOUT
+          <PowerSettingsNewIcon />
+        </Button>
+      </div>
     </div>
   );
 }
